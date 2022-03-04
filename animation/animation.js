f0ee5c6493f3cf464410bab1208fba0f79dee7e4
omotidesu <200209wi@yse-c.net>
2022-03-04 10:39:52 +0900

    animation追加

---
+++ animation/animation.js
@@ -0,0 +1,26 @@
+
+var monster = $('#monster')
+console.log(monster)
+
+function show() {
+    monster.fadeIn()
+}
+
+function hide() {
+    monster.fadeOut()
+}
+
+function moveRight() {
+}
+
+function moveLeft() {
+}
+
+function moveUp() {
+}
+
+function moveDown() {
+}
+
+function moveSequence() {
+}
